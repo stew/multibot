c71fe124caae06f2814bd95a8b37c66caad94d98
OlegYch <olegych@tut.by>
2015-03-04 01:51:50 +0300

    add gitter

--- src/main/scala/org/multibot/Multibot.scala
+++ src/main/scala/org/multibot/Multibot.scala
@@ -13,13 +13,17 @@ object Cmd {
   def unapply(s: String) = if (s.contains(' ')) Some(s.split(" ", 2).toList) else None
 }
 
-case class Multibot(cache: InterpretersCache, botname: String, channels: List[String]) {
+case class Multibot(cache: InterpretersCache, botname: String, channels: List[String],
+                    settings: Builder[PircBotX] => Builder[PircBotX] = identity) {
   val NUMLINES = 5
   val LAMBDABOT = "lambdabot"
   val ADMINS = List("imeredith", "lopex", "tpolecat", "OlegYch")
   val httpHandler = HttpHandler()
   def start() {
-    bot.startBot()
+    new Thread(){
+      override def run(): Unit = bot.startBot()
+      start()
+    }
   }
 
   private def timeout[T](line: String)(f: String => T): T = {
@@ -48,7 +52,7 @@ case class Multibot(cache: InterpretersCache, botname: String, channels: List[St
       timeout.tryComplete(Success(false))
     }
   }
-  private val builder = new Builder[PircBotX].
+  private val builder = settings(new Builder[PircBotX].
     setName(botname).setEncoding(Charset.forName("UTF-8")).setAutoNickChange(true).setAutoReconnect(true)
     .setServerHostname("irc.freenode.net")
     .setShutdownHookEnabled(false)
@@ -81,7 +85,7 @@ case class Multibot(cache: InterpretersCache, botname: String, channels: List[St
       super.onMessage(event)
       handle(event.getChannel.getName, event.getUser.getNick, event)
     }
-  })
+  }))
   channels.foreach(builder.addAutoJoinChannel(_))
   private case object bot extends org.pircbotx.PircBotX(builder.buildConfiguration())
 }
--- src/main/scala/org/multibot/Multibottest.scala
+++ src/main/scala/org/multibot/Multibottest.scala
@@ -1,15 +1,24 @@
 package org.multibot
 
-object Multibottest extends App {
-  val cache = InterpretersCache(List("#scala", "#scalaz"))
-  val PRODUCTION = Option(System getenv "multibot.production") exists (_.toBoolean)
-  val BOTNAME = if (PRODUCTION) "multibot_" else "multibot__"
-  val channels: List[String] = if (PRODUCTION)
-    List("#clojure.pl", "#scala.pl", "#jruby", "#ruby.pl", "#rubyonrails.pl", "#scala", "#scalaz", "#scala-fr", "#lift", "#playframework", "#bostonpython", "#fp-in-scala", "#CourseraProgfun", "#shapeless", "#akka", "#sbt", "#scala-monocle", "#scala-ru")
-  else
-    List("#multibottest", "#multibottest2")
+import javax.net.ssl.SSLSocketFactory
 
-  Multibot(cache, BOTNAME, channels).start()
+object Multibottest extends App {
+  val cache = InterpretersCache(List("#scala", "#scalaz", "#dev-ua/scala"))
+  val PRODUCTION = Option(System getenv "multibot.production") exists (_.toBoolean)
+  val gitterPass = Option(System getenv "multibot.gitter.pass").getOrElse("709182327498f5ee393dbb0bc6e440975fa316e5")
+  Multibot(cache, if (PRODUCTION) "multibot_" else "multibot__",
+    if (PRODUCTION)
+      List("#clojure.pl", "#scala.pl", "#scala", "#scalaz", "#scala-fr", "#lift", "#playframework",
+        "#bostonpython", "#fp-in-scala", "#CourseraProgfun", "#shapeless", "#akka", "#sbt", "#scala-monocle")
+    else
+      List("#multibottest", "#multibottest2")
+  ).start()
+  Multibot(cache,
+    if (PRODUCTION) "multibot1" else "multibot2",
+    if (PRODUCTION) List("#dev-ua/scala") else List("#OlegYch/multibot"),
+  _.setServerHostname("irc.gitter.im").setServerPassword(gitterPass).
+    setSocketFactory(SSLSocketFactory.getDefault)
+  ).start()
   scala.io.StdIn.readLine()
   sys.exit()
 }
